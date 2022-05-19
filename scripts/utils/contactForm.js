413edbacf7a64dcfc01216420eb3ed1a47020e3c
STEPHANE POLETTI <polettistephane@msn.com>
2022-05-19 22:05:14 +0200

    clean code

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -6,17 +6,21 @@ const send = document.querySelector('.contact_button');
 
 
 function displayModal() {
+
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
-}
+};
 
 function closeModal() {
+
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
-}
+};
 
 send.addEventListener('click', sendMessage);
+
 function sendMessage(e) {
+
     e.preventDefault();
     console.log("message envoyé");
     console.log([firstName.value, lastName.value, email.value, message.value]);
