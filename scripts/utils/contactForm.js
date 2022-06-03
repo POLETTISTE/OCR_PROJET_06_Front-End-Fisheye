6b4589e90aebab8709beecef37cf2d3e6772c4ca
STEPHANE POLETTI <polettistephane@msn.com>
2022-06-03 09:40:18 +0200

    form aria ok

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,15 +3,17 @@ let lastName = document.querySelector('#nom');
 let email = document.querySelector('#email');
 let message = document.querySelector('#message');
 const send = document.querySelector('.contact_button');
-const modal = document.getElementById("contact_modal");
+const contactModal = document.getElementById("contact_modal");
+const modal = document.getElementById("modal");
+const modalCloseButton = document.querySelector("#header-form > div > img")
 
 function displayModal() {
-	modal.style.display = "block";
+    contactModal.style.display = "block";
+    modal.focus();
 };
 
 function closeModal() {
     modal.style.display = "none";
-
 };
 
 document.addEventListener('keydown', EscapeModal);
@@ -19,9 +21,23 @@ document.addEventListener('keydown', EscapeModal);
 function EscapeModal(e) {
     if(e.key === "Escape") {
         e.preventDefault();
+        e.stopPropagation();
         closeModal();           
     }
 };
+
+
+
+modalCloseButton.addEventListener('keydown', EscapeModalEnter);
+
+function EscapeModalEnter(e) {
+    if(e.key === "Enter") {
+        e.preventDefault();
+        e.stopPropagation();
+        closeModal();           
+    }
+
+}
 
 send.addEventListener('click', sendMessage);
 
@@ -34,8 +50,8 @@ function sendMessage(e) {
     lastName.value = "";
     email.value = "";
     message.value = "";
-
     closeModal();
 };
+
 
 
